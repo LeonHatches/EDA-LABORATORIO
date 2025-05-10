bccebc74aa78e40b946a127e38d4d7dc1d00e3bc
leonhatches <jhatches@unsa.edu.pe>
2025-05-10 17:03:06 -0500

    | Laboratorio 01 | Problema 02 | : Creación de la estructura del programa

---
+++ Laboratorio-01/Problema2.java
@@ -0,0 +1,21 @@
+/* 
+ * Implementa un programa en Java que encuentre todos los números primos en un
+ * rango definido por el usuario utilizando el algoritmo de la Criba de
+ * Eratóstenes.
+ */
+
+import java.util.Scanner;
+
+
+public class Problema2 {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main (String [] args) {
+
+    }
+
+
+    public static void mostrar ( boolean numeros []) {
+
+    }
+}
