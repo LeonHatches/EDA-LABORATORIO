932e68e641ee16faccd31ff3d1e32160a9a00083
leonhatches <jhatches@unsa.edu.pe>
2025-05-09 21:41:42 -0500

    | Laboratorio 01 | Problema 01 | : Creación de la estructura del programa

---
+++ Laboratorio-01/Problema1.java
@@ -0,0 +1,36 @@
+/* 
+ * Desarrolla un programa en Java que implemente un sistema de gestión de
+ * calificaciones de estudiantes. El programa debe permitir al usuario ingresar
+ * las calificaciones de N estudiantes y calcular la mediana, moda y desviació
+ * estándar.
+ */
+
+public class Problema1 {
+    public static void main (String [] args) {
+        
+    }
+
+    public static void ingresar (int [] notas) {
+
+    }
+
+    public static double hallarMediana (int [] notas) {
+        return 0;
+    }
+
+    public static void ordenar (int [] notas) {
+        
+    }
+
+    public static int hallarModa (int [] notas) {
+        return 0;
+    }
+
+    public static double hallarDesviacion (int [] notas) {
+        return 0;
+    }
+
+    public static double hallarPromedio (int [] notas) {
+        return 0;
+    }
+}
