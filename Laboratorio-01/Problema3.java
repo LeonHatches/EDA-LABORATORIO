6d4ee619b887d66367708b85dacafab52a194c37
leonhatches <jhatches@unsa.edu.pe>
2025-05-10 19:15:23 -0500

    | Laboratorio 01 | Problema 03 | : Se crea la función "mostrar()" y el arreglo definido para probar el Algoritmo de Ordenamiento

--- Laboratorio-01/Problema3.java
+++ Laboratorio-01/Problema3.java
@@ -8,11 +8,20 @@ public class Problema3 {
 
     public static void main (String [] args) {
 
+        // Arreglo del 1 al 10 desordenado
+        int [] numeros = {7, 10, 6, 2, 9, 8, 1, 3, 5, 4};
+
+        mostrar(numeros);
     }
     
 
     public static void mostrar (int [] numeros) {
+        
+        System.out.print("El arreglo con la inserción es: _");
 
+        for (int n : numeros) {
+            System.out.print(n + "_");
+        }
     }
 
 
