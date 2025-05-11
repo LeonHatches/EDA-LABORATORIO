15175dd2471f155f879ec792057c18ac46f10efd
leonhatches <jhatches@unsa.edu.pe>
2025-05-10 19:09:56 -0500

    | Laboratorio 01 | Problema 03 | : Creación de la estructura del programa

---
+++ Laboratorio-01/Problema3.java
@@ -0,0 +1,23 @@
+/*
+ * Desarrolla un algoritmo que implemente el Ordenamiento por Inserción,
+ * asegurando que en cada paso del bucle el segmento procesado de la lista
+ * permanece ordenado (principio de invariante).
+ */
+
+public class Problema3 {
+
+    public static void main (String [] args) {
+
+    }
+    
+
+    public static void mostrar (int [] numeros) {
+
+    }
+
+
+    // ALGORITMO DE INSERCIÓN
+    public static void insercion (int [] numeros) {
+
+    }
+}
