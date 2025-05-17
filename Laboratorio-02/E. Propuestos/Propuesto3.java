5a176ca44b741a150e3be4a6b77b88cfd2f84447
leonhatches <jhatches@unsa.edu.pe>
2025-05-17 16:17:26 -0500

    | Laboratorio 02 | E. Propuesto 03 | : Se crea la lógica a seguir en un comentario

--- Laboratorio-02/E. Propuestos/Propuesto3.java
+++ Laboratorio-02/E. Propuestos/Propuesto3.java
@@ -1,6 +1,10 @@
 public class Propuesto3 {
     public void trianguloRecursivo1 (int base) {
-
+        
+        /* Siempre y cuando base se > 0, se llama al triángulo
+         * recursivo y luego con un for se imprime "*" según
+         * cantidad de "base" 
+         */
     }
 
     public static void main (String [] args) {
