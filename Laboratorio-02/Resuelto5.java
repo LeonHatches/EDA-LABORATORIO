7904d77ff5fe4402a958a38d0354db9bd401de27
leonhatches <jhatches@unsa.edu.pe>
2025-05-17 00:24:16 -0500

    | Laboratorio 02 | E. Resuelto 05 | : Se prueba el código Resuelto

---
+++ Laboratorio-02/Resuelto5.java
@@ -0,0 +1,16 @@
+public class Resuelto5 {
+    int factorial(int fact) {
+        if (fact > 0) {
+            int valor = fact * factorial(fact - 1);
+            return valor;
+        } else {
+            return 1;
+        }
+    }
+
+    public static void main(String[] ar) {
+        Resuelto5 re = new Resuelto5();
+        int f = re.factorial(4);
+        System.out.println("El factorial de 4 es " + f);
+    }
+}
