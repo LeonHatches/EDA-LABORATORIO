2685cd299552d1003794e694455eb3b494774a63
leonhatches <jhatches@unsa.edu.pe>
2025-05-17 00:05:27 -0500

    | Laboratorio 02 | E. Resuelto 01 | : Se prueba el código Resuelto

---
+++ Laboratorio-02/Resuelto1.java
@@ -0,0 +1,10 @@
+public class Resuelto1 {
+    void repetir() {
+        repetir();
+    }
+
+    public static void main(String[] ar) {
+        Resuelto1 re = new Resuelto1();
+        re.repetir();
+    }
+}
