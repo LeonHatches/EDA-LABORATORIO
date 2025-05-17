6438a70f41981232506f0fe4ded8019fcd5a0721
leonhatches <jhatches@unsa.edu.pe>
2025-05-17 15:58:35 -0500

    | Laboratorio 02 | E. Propuesto 03 | : Se coloca la firma propuesta

---
+++ Laboratorio-02/E. Propuestos/Propuesto3.java
@@ -0,0 +1,11 @@
+public class Propuesto3 {
+    public void trianguloRecursivo1 (int base) {
+
+    }
+
+    public static void main (String [] args) {
+        Propuesto3 re = new Propuesto3();
+
+        re.trianguloRecursivo1(5);
+    }
+}
