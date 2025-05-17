a5e7b03c543e5c5af114ec3a4f9a9afb8850e57c
leonhatches <jhatches@unsa.edu.pe>
2025-05-17 14:49:31 -0500

    | Laboratorio 02 | E. Propuesto 02 | : Se desarrolla la lógica en comentarios

--- Laboratorio-02/E. Propuestos/Propuesto2.java
+++ Laboratorio-02/E. Propuestos/Propuesto2.java
@@ -1,7 +1,22 @@
 public class Propuesto2 {
     
     public int [] rotarIzquierdaArray (int [] A, int d) {
-        return A;
+        
+        /* CASO BASE: Va constar de un arreglo de los
+         * elementos que no se rotan SI es que el
+         * tamaño del arreglo y el d son iguales
+        */
+
+        /* En otro caso, se va crea el Ainvertido según
+         * el tamaño de A que recibe, luego se asigna al
+         * indice 0 el elemento de posición "d", para
+         * después enviar un arreglo de 1 a length para
+         * iniciar la recursión.
+         * Al final solo se asignan los valores al
+         * arreglo a mostrar.
+         */
+        int [] Ainvertido = new int [A.length];
+        return Ainvertido;
     }
 
     public static void main (String [] args) {
