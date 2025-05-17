d2f405e2c86c30ea6e579015bdd47639de79a679
leonhatches <jhatches@unsa.edu.pe>
2025-05-17 00:45:58 -0500

    | Laboratorio 02 | E. Propuesto 01 | : Se coloca la funcionalidad del método

---
+++ Laboratorio-02/E. Propuestos/Propuesto1.java
@@ -0,0 +1,13 @@
+public class Propuesto1 {
+    public int [] invertirArray (int [] A) {
+        return A;
+    }
+
+    
+    public static void main (String [] args) {
+        // Crear Arreglo
+        // Mostrar Arreglo
+        // Invertir Arreglo
+        // Mostrar Arreglo
+    }
+}
