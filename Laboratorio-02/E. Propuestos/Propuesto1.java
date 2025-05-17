54de2549c9523c2da02b891415a8499e2879d0e0
leonhatches <jhatches@unsa.edu.pe>
2025-05-17 11:16:42 -0500

    | Laboratorio 02 | E. Propuesto 01 | : Se explica la lógica (resolución) del método

--- Laboratorio-02/E. Propuestos/Propuesto1.java
+++ Laboratorio-02/E. Propuestos/Propuesto1.java
@@ -1,8 +1,24 @@
 public class Propuesto1 {
+    
     public int [] invertirArray (int [] A) {
-        if (A.length == 1) {
+        
+        int Asalida [] = new int [A.length];
+        
+        if (false) {
+            /* Se implementa una base que sea en dos casos, con un arreglo de tamaño 1 y 2.
+             * En dado caso sea 1, se devolverá el arreglo unitario.
+             * En dado caso sea 2, se devolverá el arreglo con elementos intercambiados.
+            */
             return A;
         }
+        
+        /* En esta parte, se harán varios procesos.
+         * Primero, se crea un arreglo de RECURSIÓN (Diferente al de Salida)
+         * Segundo, se colocan (el primero en el último y viceversa) en el arreglo de SALIDA.
+         * Tercero, se usa un for para preparar el arreglo de RECURSIÓN sin los anteriores datos (0, length - 1)
+         * Cuarto, se usa el arreglo RECIBIDO para añadirlo al arreglo de SALIDA
+         * Quinto, se envia.
+        */
 
         return A;
     }
