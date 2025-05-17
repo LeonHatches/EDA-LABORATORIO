16ec3e9952a29ebcfefa5bc48844123c70bcf15a
leonhatches <jhatches@unsa.edu.pe>
2025-05-17 01:22:38 -0500

    | Laboratorio 02 | E. Propuesto 01 | : Se trabaja el main para mostrar, invertir y volver a mostrar

--- Laboratorio-02/E. Propuestos/Propuesto1.java
+++ Laboratorio-02/E. Propuestos/Propuesto1.java
@@ -1,13 +1,29 @@
 public class Propuesto1 {
     public int [] invertirArray (int [] A) {
+        if (A.length == 1) {
+            return A;
+        }
+
         return A;
     }
 
     
     public static void main (String [] args) {
-        // Crear Arreglo
-        // Mostrar Arreglo
-        // Invertir Arreglo
-        // Mostrar Arreglo
+        
+        int [] arreglo = {11, 7, 5, 9, 12, 6, 10};
+        Propuesto1 re = new Propuesto1();
+
+        // MOSTRAR
+        for (int i : arreglo)
+            System.out.print (i+" ");
+        System.out.println();
+        
+        // INVERTIR
+        int [] arregloInv = re.invertirArray(arreglo);
+
+        // MOSTRAR
+        for (int i : arregloInv) {
+            System.out.print (i+" ");
+        }
     }
 }
