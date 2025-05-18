89a27aae19fe0b5ed171e511a4073ef55b5e56d3
leonhatches <jhatches@unsa.edu.pe>
2025-05-17 20:02:53 -0500

    | Laboratorio 02 | E. Propuesto 06 | : Se coloca la firma del Problema

---
+++ Laboratorio-02/E. Propuestos/Propuesto6.java
@@ -0,0 +1,11 @@
+public class Propuesto6 {
+    public void cuadradoRecursivo (int base) {
+
+    }
+
+    public static void main (String [] args) {
+        Propuesto6 re = new Propuesto6();
+
+        re.cuadradoRecursivo(5);
+    }
+}
