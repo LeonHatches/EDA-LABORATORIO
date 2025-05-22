327566ecb98c4cb3c97fb52747ab2d529a356e75
leonhatches <jhatches@unsa.edu.pe>
2025-05-22 08:05:33 -0500

    | Laboratorio 03 | E. Resuelto 01 y 02 | : Se ejecuta el código propuesto

---
+++ Laboratorio-03/Resueltos/Resuelto1Y2.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Resuelto1Y2 {
+    public static void main(String[] args) {
+        ArrayList<String> alumnos = new ArrayList<String>();
+        ArrayList<Integer> notas = new ArrayList<Integer>();
+        alumnos.add("MARIA");
+        alumnos.add("DIEGO");
+        alumnos.add("RENE");
+        alumnos.add("ALONSO");
+        System.out.println(alumnos.hashCode());
+        System.out.println(alumnos.isEmpty());
+        System.out.println(alumnos.size());
+        
+        Iterator<String> itA = alumnos.iterator();
+        
+        while (itA.hasNext()) {
+            System.out.println(itA.next());
+        } 
+    }
+}
