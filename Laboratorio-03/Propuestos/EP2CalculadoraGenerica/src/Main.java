c2ae4b9e59763a0ea2a03440f17f516ebf9a3db0
leonhatches <jhatches@unsa.edu.pe>
2025-05-24 06:27:51 -0500

    | Laboratorio 03 | EP2 Calculadora | Main | : Se crea la estructura del menú a implementar

--- Laboratorio-03/Propuestos/EP2CalculadoraGenerica/src/Main.java
+++ Laboratorio-03/Propuestos/EP2CalculadoraGenerica/src/Main.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int opcion = 0;
+
+        do {
+
+            switch (opcion) {
+                case 1:
+                    break;
+                
+                case 2:
+                    break;            
+                
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+
+                case 6:
+                    break;
+
+                case 7:
+                    break;
+
+                case 8:
+                    break;
+
+                default:
+                    System.out.println("Opción invalida - Intente de nuevo.\n");
+                    break;
+            }
+
+        } while (opcion != 8);
     }
 }
