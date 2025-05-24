541796b1bae86a6dd371ff9749c706e112ba869d
leonhatches <jhatches@unsa.edu.pe>
2025-05-24 11:08:42 -0500

    | Laboratorio 03 | EP1 Listas | Node | : Se crean los dos atributos con setters y getters

--- Laboratorio-03/Propuestos/EP1Listas/src/Node.java
+++ Laboratorio-03/Propuestos/EP1Listas/src/Node.java
@@ -1,3 +1,20 @@
 public class Node <T> {
-    
+    private T data;
+    private Node <T> nextNode;
+
+    public void setData (T data) {
+        this.data = data;
+    }
+
+    public void setNext (Node<T> nextNode) {
+        this.nextNode = nextNode;
+    }
+
+    public T getData () {
+        return data;
+    }
+
+    public Node<T> getNext () {
+        return nextNode;
+    }
 }
