a144a10ddeaa848d13f88d86f992336e68b542e7
leonhatches <jhatches@unsa.edu.pe>
2025-05-24 11:12:22 -0500

    | Laboratorio 03 | EP1 Listas | Node | : Se crean constructores y toString

--- Laboratorio-03/Propuestos/EP1Listas/src/Node.java
+++ Laboratorio-03/Propuestos/EP1Listas/src/Node.java
@@ -2,6 +2,15 @@ public class Node <T> {
     private T data;
     private Node <T> nextNode;
 
+    public Node (T data, Node <T> nextNode) {
+        this.data = data;
+        this.nextNode = nextNode;
+    }
+
+    public Node (T data) {
+        this(data, null);
+    }
+
     public void setData (T data) {
         this.data = data;
     }
@@ -16,5 +25,9 @@ public class Node <T> {
 
     public Node<T> getNext () {
         return nextNode;
+    }
+
+    public String toString () {
+        return data.toString();
     }
 }
