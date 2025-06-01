a6a52fc96a81107c2981263a299d1eb04c13f911
leonhatches <jhatches@unsa.edu.pe>
2025-05-31 22:34:01 -0500

    | Laboratorio 04 | EP1 | : Se crea el método deleteAtPosition

--- Laboratorio-04/Propuestos/EP1/DoublyLinkedList.java
+++ Laboratorio-04/Propuestos/EP1/DoublyLinkedList.java
@@ -18,7 +18,42 @@ public class DoublyLinkedList <E> implements Link<E> {
     public void insert (int index, E data) {}
     public void printList () {}
     public void deleteByKey (E key) {}
-    public void deleteAtPosition (int index) {}
+
+    public void deleteAtPosition (int index) {
+        if (index >= 0 && index < size) {
+            
+            if (index == 0)
+                removeFirst();
+            
+            else if (index == size - 1)
+                removeLast();
+            
+            else if (size - index < index) {
+                Node<E> aux = last;
+
+                for (int i = size - 1 ; i > index ; i--) {
+                    aux = aux.getPrev();
+                }
+
+                aux.getPrev().setNext(aux.getNext());
+                aux.getNext().setPrev(aux.getPrev());
+                --size;
+
+            } else {
+                Node<E> aux = first;
+
+                for (int i = 0 ; i < index ; i++) {
+                    aux = aux.getNext();
+                }
+
+                aux.getPrev().setNext(aux.getNext());
+                aux.getNext().setPrev(aux.getPrev());
+                --size;
+
+            }
+            
+        }
+    }
     
     public void removeFirst () {
         if (size != 0) {
