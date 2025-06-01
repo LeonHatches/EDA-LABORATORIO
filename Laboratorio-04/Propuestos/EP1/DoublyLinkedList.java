9026ace6a48f60ac0c91b429dd6df1d8bb4471af
leonhatches <jhatches@unsa.edu.pe>
2025-05-31 20:19:39 -0500

    | Laboratorio 04 | EP1 | : Se crea la base del DoublyLinkedList

---
+++ Laboratorio-04/Propuestos/EP1/DoublyLinkedList.java
@@ -0,0 +1,11 @@
+public class DoublyLinkedList <E> implements Link<E> {
+    public int size () {return 0;}
+    public void insert(E data) {}
+    public void printList() {}
+    public void deleteByKey(E key) {}
+    public void deleteAtPosition(int position) {}
+    public void removeFirst() {}
+    public void removelast() {}
+    public void addFirst() {}
+    public void addLast() {}
+}
