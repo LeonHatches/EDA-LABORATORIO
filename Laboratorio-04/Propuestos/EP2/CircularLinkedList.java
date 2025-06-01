74895b0e8438dce3cdd0250ef8a701609181c535
leonhatches <jhatches@unsa.edu.pe>
2025-05-31 23:51:11 -0500

    | Laboratorio 04 | EP2 | : Se crean los métodos size y printList

--- Laboratorio-04/Propuestos/EP2/CircularLinkedList.java
+++ Laboratorio-04/Propuestos/EP2/CircularLinkedList.java
@@ -9,13 +9,32 @@ public class CircularLinkedList<E> implements Link<E> {
         this.size = 0;
     }
     
-    public int size() {return 0;}
+    public int size() {
+        return size;
+    }
+
     public void insert(int index, E data) {}
-    public void printList() {}
+    
+    public void printList() {
+        Node<E> aux = head;
+
+        for (int i = 0 ; i < size ; i++) {
+            System.out.print(head.getData()+" ");
+            aux = aux.getNext();
+        }
+        System.out.println();
+    }
+
     public void deleteByKey(E key) {}
     public void deleteAtPosition(int index) {}
     public void removeFirst() {}
     public void removeLast() {}
-    public void addFirst(E data) {}
-    public void addLast(E data) {}
+
+    public void addFirst (E data) {
+
+    }
+
+    public void addLast (E data) {
+
+    }
 }
