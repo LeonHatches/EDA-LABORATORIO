915de73f06e9f036d5f3c977071b61b9029ba5c6
leonhatches <jhatches@unsa.edu.pe>
2025-05-31 23:43:49 -0500

    | Laboratorio 04 | EP2 | : Se crea el constructor

--- Laboratorio-04/Propuestos/EP2/CircularLinkedList.java
+++ Laboratorio-04/Propuestos/EP2/CircularLinkedList.java
@@ -1,5 +1,21 @@
 package EP2;
 
-public class CircularLinkedList {
+public class CircularLinkedList<E> implements Link<E> {
+    private Node<E> head;
+    private int size;
 
+    public CircularLinkedList () {
+        this.head = null;
+        this.size = 0;
+    }
+    
+    public int size() {return 0;}
+    public void insert(int index, E data) {}
+    public void printList() {}
+    public void deleteByKey(E key) {}
+    public void deleteAtPosition(int index) {}
+    public void removeFirst() {}
+    public void removeLast() {}
+    public void addFirst(E data) {}
+    public void addLast(E data) {}
 }
