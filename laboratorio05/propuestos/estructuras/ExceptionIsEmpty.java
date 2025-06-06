728a8681c3f4bf1b857f7efcfb9b742a5f0a01ab
leonhatches <jhatches@unsa.edu.pe>
2025-06-06 10:25:55 -0500

    | Laboratorio 05 | Propuestos | Estructuras | : Se crea una clase excepción para manejar estructuras vacías

---
+++ laboratorio05/propuestos/estructuras/ExceptionIsEmpty.java
@@ -0,0 +1,10 @@
+package laboratorio05.propuestos.estructuras;
+
+public class ExceptionIsEmpty extends Exception {
+	public ExceptionIsEmpty() {
+		super();
+	}
+	public ExceptionIsEmpty(String msg) {
+		super(msg);
+	}
+}
