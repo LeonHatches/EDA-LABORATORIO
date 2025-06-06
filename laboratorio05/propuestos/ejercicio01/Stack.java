2e3797a12b7ab15ac4d4756802346ad3a996d967
leonhatches <jhatches@unsa.edu.pe>
2025-06-05 22:29:35 -0500

    | Laboratorio 05 | Propuestos | EJ1 | : Se crea la clase Stack con atributos (tope, capacidad y cantidad) y métodos autogenerados

---
+++ laboratorio05/propuestos/ejercicio01/Stack.java
@@ -0,0 +1,58 @@
+package laboratorio05.propuestos.ejercicio01;
+import laboratorio05.propuestos.estructuras.*;
+
+public class Stack<E> implements Pila<E> {
+
+    private Node<E> tope;
+    private int     capacidad;
+    private int     cantidad;
+
+    public Stack (int capacidad) {
+        tope           = new Node<E>(null);
+        this.capacidad = capacidad;
+        this.cantidad  = 0;
+    }
+
+    @Override
+    public void push(E item) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'push'");
+    }
+
+    @Override
+    public E pop() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pop'");
+    }
+
+    @Override
+    public E top() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'top'");
+    }
+
+    @Override
+    public void destroystak() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'destroystak'");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+    }
+
+    @Override
+    public boolean isFull() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isFull'");
+    }
+
+    @Override
+    public void printStack() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'printStack'");
+    }
+    
+}
