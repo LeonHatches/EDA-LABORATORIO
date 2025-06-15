f15362dbc320a12c8b3f12d110d206c53e8f49b5
leonhatches <jhatches@unsa.edu.pe>
2025-06-14 19:54:44 -0500

    | Laboratorio 06 | Propuestos | Estructuras | : Se crean excepciones para item no encontrado y duplicado

---
+++ laboratorio06/propuestos/estructuras/ItemDuplicated.java
@@ -0,0 +1,12 @@
+package laboratorio06.propuestos.estructuras;
+
+public class ItemDuplicated extends Exception {
+    public ItemDuplicated () {
+        super();
+    }
+
+    public ItemDuplicated (String msg) {
+        super(msg);
+    }
+}
+
---
+++ laboratorio06/propuestos/estructuras/ItemNotFound.java
@@ -0,0 +1,12 @@
+package laboratorio06.propuestos.estructuras;
+
+public class ItemNotFound extends Exception {
+    public ItemNotFound () {
+        super();
+    }
+
+    public ItemNotFound (String msg) {
+        super(msg);
+    }
+}
+
