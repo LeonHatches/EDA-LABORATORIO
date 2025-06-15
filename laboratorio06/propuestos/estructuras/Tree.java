f26eab1bb3bdd535a47ad492c165cedac04fabbc
leonhatches <jhatches@unsa.edu.pe>
2025-06-15 17:06:05 -0500

    | Laboratorio 06 | Propuestos | Estructuras | : Se agrega el remove a la interfaz

--- laboratorio06/propuestos/estructuras/Tree.java
+++ laboratorio06/propuestos/estructuras/Tree.java
@@ -4,6 +4,7 @@ public interface Tree <T extends Comparable<T>> {
     void destroy ();
     boolean isEmpty ();
     void insert (T data) throws ItemDuplicated;
+    void remove (T data) throws ItemNotFound;
     T search (T data) throws ItemNotFound;
     T min () throws ExceptionIsEmpty;
     T max () throws ExceptionIsEmpty;
