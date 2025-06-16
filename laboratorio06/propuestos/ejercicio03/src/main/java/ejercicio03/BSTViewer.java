772f40782ac715e7398ee7af26fc1e791f51ea74
leonhatches <jhatches@unsa.edu.pe>
2025-06-16 18:17:02 -0500

    | Laboratorio 06 | Propuestos | Ejercicio 03 | : Se crea la estructura que se usará para crear el gráfico

--- laboratorio06/propuestos/ejercicio03/src/main/java/ejercicio03/BSTViewer.java
+++ laboratorio06/propuestos/ejercicio03/src/main/java/ejercicio03/BSTViewer.java
@@ -1,7 +1,18 @@
 package ejercicio03;
 import org.graphstream.graph.*;
+import org.graphstream.graph.implementations.*;
 import estructuras.*;
 
 public class BSTViewer<T extends Comparable<T>> extends BST<T>{
     private Graph graph;
+
+    public BSTViewer() {
+        super();
+        graph = new SingleGraph("BST");
+        System.setProperty("org.graphstream.ui", "swing");
+    }
+
+    public void graphTree () throws ExceptionIsEmpty {
+
+    }
 }
