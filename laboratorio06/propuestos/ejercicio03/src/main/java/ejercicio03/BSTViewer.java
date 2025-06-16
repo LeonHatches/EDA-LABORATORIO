6bcd239d8320d13a11a9d8b346437b777ed317c7
leonhatches <jhatches@unsa.edu.pe>
2025-06-16 18:58:27 -0500

    | Laboratorio 06 | Propuestos | Ejercicio 03 | : Se crean dos métodos para graphTree, público y privado, donde se recorre en PreOrden

--- laboratorio06/propuestos/ejercicio03/src/main/java/ejercicio03/BSTViewer.java
+++ laboratorio06/propuestos/ejercicio03/src/main/java/ejercicio03/BSTViewer.java
@@ -1,7 +1,9 @@
 package ejercicio03;
 import org.graphstream.graph.*;
 import org.graphstream.graph.implementations.*;
-import estructuras.*;
+import estructuras.Node;
+import estructuras.BST;
+import estructuras.ExceptionIsEmpty;
 
 public class BSTViewer<T extends Comparable<T>> extends BST<T>{
     private Graph graph;
@@ -13,6 +15,23 @@ public class BSTViewer<T extends Comparable<T>> extends BST<T>{
     }
 
     public void graphTree () throws ExceptionIsEmpty {
+        graphTree(getRoot(), graph, null);
+        graph.display();
+    }
 
+    private void graphTree (Node<T> actual, Graph graph, String fatherId) {
+        
+        if (actual != null) {
+
+            String actualId = actual.getData().toString();
+            graph.addNode(actualId).setAttribute("ui.label", actualId);
+            
+            if (fatherId != null)
+                graph.addEdge(fatherId + "-" + actualId, fatherId, actualId);
+
+            graphTree(actual.getLeft(), graph, fatherId);
+            graphTree(actual.getRight(), graph, fatherId);
+        }
     }
 }
+
