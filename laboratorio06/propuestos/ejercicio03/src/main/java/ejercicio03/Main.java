3ea00635f278271874c298a88a42c1981407a534
leonhatches <jhatches@unsa.edu.pe>
2025-06-16 20:13:26 -0500

    | Laboratorio 06 | Propuestos | Ejercicio 03 | : Se crea un Main que inserta elementos del ejercicio01

--- laboratorio06/propuestos/ejercicio03/src/main/java/ejercicio03/Main.java
+++ laboratorio06/propuestos/ejercicio03/src/main/java/ejercicio03/Main.java
@@ -2,6 +2,27 @@ package ejercicio03;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BSTViewer<Integer> numeros = new BSTViewer<>();
+
+        try {
+
+            // Insert
+            numeros.insert(8);
+            numeros.insert(3);
+            numeros.insert(10);
+            numeros.insert(1);
+            numeros.insert(6);
+            numeros.insert(4);
+            numeros.insert(7);
+            numeros.insert(14);
+            numeros.insert(13);
+            numeros.insert(20);
+
+            // Mostrar
+            numeros.graphTree();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
