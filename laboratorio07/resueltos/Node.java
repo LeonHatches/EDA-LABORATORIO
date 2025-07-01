a9c263da447452cc223ff66e1a3db9a8a2fea2b2
leonhatches <jhatches@unsa.edu.pe>
2025-07-01 00:40:28 -0500

    | Laboratorio 07 | Resueltos | : Se reutiliza el Node del Laboratorio 05 para implementar el AVL

---
+++ laboratorio07/resueltos/Node.java
@@ -0,0 +1,35 @@
+package laboratorio07.resueltos;
+
+public class Node<T> {
+    private Node<T> left, right;
+    private T data;
+
+    public Node (T data, Node<T> left, Node<T> right) {
+        this.data  = data;
+        this.left  = left;
+        this.right = right;
+    }
+
+    public Node (T data) {
+        this(data, null, null);
+    }
+
+    public Node<T> getLeft() {
+        return left;
+    }
+    public void setLeft(Node<T> left) {
+        this.left = left;
+    }
+    public Node<T> getRight() {
+        return right;
+    }
+    public void setRight(Node<T> right) {
+        this.right = right;
+    }
+    public T getData() {
+        return data;
+    }
+    public void setData(T data) {
+        this.data = data;
+    }
+}
