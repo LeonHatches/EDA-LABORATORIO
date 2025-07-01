5eb294348ae84b9f65411f0a3c21b62b011b152f
leonhatches <jhatches@unsa.edu.pe>
2025-07-01 07:01:16 -0500

    | Laboratorio 07 | Resueltos | : Se copia el código de inserción del AVL con necesidad de métodos de balance

--- laboratorio07/resueltos/AVLTree.java
+++ laboratorio07/resueltos/AVLTree.java
@@ -14,4 +14,49 @@ public class AVLTree<E extends Comparable<E>> extends BST<E> {
             return getData().toString();
         }
     }
+
+    private boolean height;
+    private NodeAVL<E> root;
+
+    public void insert(E x) throws ItemDuplicated {
+        this.height = false;
+        this.root = insert(x, (NodeAVL<E>) this.root);
+    }
+
+    protected NodeAVL<E> insert(E x, NodeAVL<E> node) throws ItemDuplicated {
+        NodeAVL<E> fat = node;
+
+        if (node == null) {
+            this.height = true;
+            fat = new NodeAVL<E>(x);
+        
+        } else {
+            int resC = node.getData().compareTo(x);
+
+            if (resC == 0) throw new ItemDuplicated("Duplicado");
+            if (resC  < 0) {
+                fat.setRight( insert(x, (NodeAVL<E>) node.getRight()));
+                if (this.height) {
+                    switch (fat.bf) {
+                        case -1:
+                            fat.bf = 0;
+                            this.height = false;
+                            break;
+                    
+                        case 0:
+                            fat.bf = 1;
+                            break;
+                        
+                        case 1:
+                            //fat = balanceToLeft(fat);
+                            this.height = false;
+                            break;
+                    }
+                }
+            } else {
+                //
+            }
+        }
+        return fat;
+    }
 }
