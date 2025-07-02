59147d5cef05c547522d4c2fec33099474ca7cab
leonhatches <jhatches@unsa.edu.pe>
2025-07-01 22:07:47 -0500

    | Laboratorio 07 | Propuestos | Estructuras | : Se copia el BALANCEO IZQ y DER con correciones según el EJERCICIO 1

--- laboratorio07/propuestos/estructuras/AVLTree.java
+++ laboratorio07/propuestos/estructuras/AVLTree.java
@@ -29,4 +29,76 @@ public class AVLTree<E extends Comparable<E>> extends BST<E> {
         node = p;
         return node;
     }
+
+        private Node<E> balanceToLeft (Node<E> node) {
+        Node<E> hijo = (Node<E>) node.getRight();
+
+        switch (hijo.bf) {
+            case 1:
+                node.bf = 0;
+                hijo.bf = 0;
+                node = rotacionSimpleIzquierda(node);
+                break;
+        
+            case -1:
+                Node<E> nieto = (Node<E>) hijo.getLeft();
+                switch (nieto.bf) {
+                    case -1:
+                        node.bf = 0;
+                        hijo.bf = 1;
+                        break;
+                
+                    case 0:
+                        node.bf = 0;
+                        hijo.bf = 0;
+                        break;
+                    
+                    case 1:
+                        node.bf = -1;
+                        hijo.bf = 0;
+                        break;
+                }
+                nieto.bf = 0;
+                node.setRight(rotacionSimpleDerecha(hijo));
+                node = rotacionSimpleIzquierda(node);
+                break;
+        }
+        return node;
+    }
+
+     private Node<E> balanceToRight (Node<E> node) {
+        Node<E> hijo = (Node<E>) node.getLeft();
+
+        switch (hijo.bf) {
+            case -1:
+                node.bf = 0;
+                hijo.bf = 0;
+                node = rotacionSimpleDerecha(node);
+                break;
+        
+            case 1:
+                Node<E> nieto = (Node<E>) hijo.getRight();
+                switch (nieto.bf) {
+                    case -1:
+                        node.bf = 1;
+                        hijo.bf = 0;  
+                        break;
+                
+                    case 0:
+                        node.bf = 0;
+                        hijo.bf = 0;
+                        break;
+                    
+                    case 1:
+                        node.bf = 0;
+                        hijo.bf = -1;
+                        break;
+                }
+                nieto.bf = 0;
+                node.setLeft(rotacionSimpleIzquierda(hijo));
+                node = rotacionSimpleDerecha(node);
+                break;
+        }
+        return node;
+     }
 }
