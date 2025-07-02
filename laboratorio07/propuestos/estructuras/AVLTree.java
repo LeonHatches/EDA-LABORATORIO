1a1ce4cbbacdf93e20e9d5056c68e77fdfb2a711
leonhatches <jhatches@unsa.edu.pe>
2025-07-01 21:55:53 -0500

    | Laboratorio 07 | Propuestos | Estructuras | : Se reutiliza el código de rotaciones simples a la izquierda y derecha de los ejercicios RESUELTOS

--- laboratorio07/propuestos/estructuras/AVLTree.java
+++ laboratorio07/propuestos/estructuras/AVLTree.java
@@ -1,5 +1,32 @@
 package propuestos.estructuras;
 
 public class AVLTree<E extends Comparable<E>> extends BST<E> {
+    private boolean height;
 
+    public AVLTree () {
+        super();
+        this.height = true;
+    }
+
+    @Override
+    public void destroy () {
+        this.root = null;
+        this.height = true;
+    }
+
+    private Node<E> rotacionSimpleIzquierda (Node<E> node) {
+        Node<E> p = (Node<E>) node.getRight();
+        node.setRight(p.getLeft());
+        p.setLeft(node);
+        node = p;
+        return node;
+    }
+
+    private Node<E> rotacionSimpleDerecha (Node<E> node) {
+        Node<E> p = (Node<E>) node.getLeft();
+        node.setLeft(p.getRight());
+        p.setRight(node);
+        node = p;
+        return node;
+    }
 }
