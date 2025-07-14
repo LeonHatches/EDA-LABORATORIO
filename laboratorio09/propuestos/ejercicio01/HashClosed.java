b946a238c63dc52ee6c7b6dd18b8171a58204bae
leonhatches <jhatches@unsa.edu.pe>
2025-07-14 18:19:28 -0500

    | Laboratorio 09 | Propuestos | Ejercicio01 | : se reutiliza el código para elaborar el Hashclosed y Register

---
+++ laboratorio09/propuestos/ejercicio01/HashClosed.java
@@ -0,0 +1,78 @@
+package propuestos.ejercicio01;
+
+import java.util.LinkedList;
+
+public class HashClosed<E> {
+    private LinkedList<Register<E>> [] table;
+
+    @SuppressWarnings("unchecked")
+    public HashClosed(int capacity) {
+        table = new LinkedList[capacity];
+
+        for(int i = 0 ; i < capacity ; i++)
+            table[i] = new LinkedList<>();
+    }
+
+    private int hash(int key) {
+        return key % table.length;
+    }
+
+    public void insert(Register<E> reg) {
+        int index = hash(reg.getKey());
+        
+        for (Register<E> r : table[index]) {
+            if (r.getKey() == reg.getKey() && !r.isDeleted()) {
+                System.out.println("Clave duplicada: " + reg.getKey());
+                return;
+            }
+        }
+        table[index].add(reg);
+        System.out.println("Insertado: " + reg);
+    }
+
+    public void delete(int key) {
+        int index = hash(key);
+
+        for (Register<E> r : table[index]) {
+            if (r.getKey() == key && !r.isDeleted()) {
+                r.delete();
+                System.out.println("Eliminado lógicamente: " + key);
+                return;
+            }
+        }
+
+        System.out.println("Clave no encontrada: " + key);
+    }
+
+    public Register<E> search(int key) {
+        int index = hash(key);
+
+        for (Register<E> r : table[index]) {
+            if (r.getKey() == key && !r.isDeleted()) {
+                return r;
+            }
+        }
+
+        return null;
+    }
+
+    public void showTable() {
+        System.out.println("\n--- Estado de la Tabla Hash (Abierto) ---");
+
+        for(int i = 0 ; i < table.length ; i++) {
+            System.out.print(i + " : ");
+
+            if (table[i].isEmpty())
+                System.out.println("[VACIO]");
+            
+            else {
+                for(Register<E> r : table[i])
+                    System.out.print(r + " -> ");
+                
+                System.out.println("NULL");
+            }
+        }
+    }
+
+
+}
