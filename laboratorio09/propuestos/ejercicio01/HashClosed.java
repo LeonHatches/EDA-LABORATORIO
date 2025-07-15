d1d98f5bce15c702c99ec4fab0cdf2f03a4ce197
leonhatches <jhatches@unsa.edu.pe>
2025-07-14 19:41:08 -0500

    | Laboratorio 09 | Propuestos | Ejercicio01 | : se corrige el ingreso de claves hash duplicadas en el insert

--- laboratorio09/propuestos/ejercicio01/HashClosed.java
+++ laboratorio09/propuestos/ejercicio01/HashClosed.java
@@ -1,7 +1,5 @@
 package propuestos.ejercicio01;
 
-import java.util.LinkedList;
-
 public class HashClosed<E> {
     private Register<E> [] table;
     private int capacity;
@@ -41,58 +39,59 @@ public class HashClosed<E> {
 
         int index = hash(reg.getKey());
         
-        while ( (table[index] != null && !table[index].isDeleted())) {
+        while (table[index] != null && !table[index].isDeleted() && table[index].compareTo(reg) != 0) {
             System.out.println("Se ha detectado una colisión, se busca nuevo lugar.");
             index++;
+        }
+
+        if (table[index].compareTo(reg) == 0) {
+            System.out.println("Clave duplicada: " + reg.getKey());
+            return;
         }
 
         table[index] = reg;
         count++;
     }
 
-    public void delete(int key) {
-        int index = hash(key);
+    // public void delete(int key) {
+    //     int index = hash(key);
 
-        for (Register<E> r : table[index]) {
-            if (r.getKey() == key && !r.isDeleted()) {
-                r.delete();
-                System.out.println("Eliminado lógicamente: " + key);
-                return;
-            }
-        }
+    //     for (Register<E> r : table[index]) {
+    //         if (r.getKey() == key && !r.isDeleted()) {
+    //             r.delete();
+    //             System.out.println("Eliminado lógicamente: " + key);
+    //             return;
+    //         }
+    //     }
 
-        System.out.println("Clave no encontrada: " + key);
-    }
+    //     System.out.println("Clave no encontrada: " + key);
+    // }
 
-    public Register<E> search(int key) {
-        int index = hash(key);
+    // public Register<E> search(int key) {
+    //     int index = hash(key);
 
-        for (Register<E> r : table[index]) {
-            if (r.getKey() == key && !r.isDeleted()) {
-                return r;
-            }
-        }
+    //     while (table[index] != null && table[index].getValue().compareTo) {
 
-        return null;
-    }
+    //     }
 
-    public void showTable() {
-        System.out.println("\n--- Estado de la Tabla Hash (Abierto) ---");
+    //     return null;
+    // }
 
-        for(int i = 0 ; i < table.length ; i++) {
-            System.out.print(i + " : ");
+    // public void showTable() {
+    //     System.out.println("\n--- Estado de la Tabla Hash (Abierto) ---");
 
-            if (table[i].isEmpty())
-                System.out.println("[VACIO]");
+    //     for(int i = 0 ; i < table.length ; i++) {
+    //         System.out.print(i + " : ");
+
+    //         if (table[i].isEmpty())
+    //             System.out.println("[VACIO]");
             
-            else {
-                for(Register<E> r : table[i])
-                    System.out.print(r + " -> ");
+    //         else {
+    //             for(Register<E> r : table[i])
+    //                 System.out.print(r + " -> ");
                 
-                System.out.println("NULL");
-            }
-        }
-    }
-
-
+    //             System.out.println("NULL");
+    //         }
+    //     }
+    // }
 }
