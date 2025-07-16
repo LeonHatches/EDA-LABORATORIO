0120f0c5a99bb585ad86711674e8cc1d8d3a5c46
leonhatches <jhatches@unsa.edu.pe>
2025-07-15 19:43:44 -0500

    | Laboratorio 09 | Propuestos | Ejercicio03 | : Se crea un switch que maneje los diferentes métodos de la clase HashClosed

--- laboratorio09/propuestos/ejercicio03/Test.java
+++ laboratorio09/propuestos/ejercicio03/Test.java
@@ -5,20 +5,67 @@ import java.util.Scanner;
 public class Test {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
+        HashClosed<Integer> hash = new HashClosed<>(7);
         int opcion = 0;   
         
-        System.out.println("\n\t| TABLA HASH CERRADA (STRINGS) |");
+        System.out.println("\n\t| TABLA HASH CERRADA (INTEGER - 7 ELEMENTOS) |");
         do {
             menu();
-            System.out.print ("Ingrese opción: ");
+            
+            try {
 
-        } while (opcion != 4);
+                System.out.print ("Seleccione su opción: ");
+                opcion = sc.nextInt();
+
+                switch (opcion) {
+                    case 1:
+                        System.out.print ("Ingrese número: ");
+                        int dato = sc.nextInt();
+                        hash.insert(dato, dato);
+                        break;
+
+                    case 2:
+                        System.out.print ("Ingrese número: ");
+                        int dele = sc.nextInt();
+                        hash.delete(dele);
+                        break;
+
+                    case 3:
+                        hash.showTable();
+                        break;
+
+                    case 4:
+                        System.out.print ("Ingrese número: ");
+                        int sear = sc.nextInt();
+                        
+                        System.out.println("| ¿Elemento encontrado? | " + hash.search(sear));
+                        break;
+                    
+                    case 5:
+                        System.out.println("Saliendo del programa...");
+                        break;
+
+                    default:
+                        System.out.println("Ingrse una opción váida.");
+                        break;
+                }
+            
+            } catch (Exception e) {
+                System.out.println("Ha ocurrido un error...");
+                sc.nextLine();
+            }
+
+        } while (opcion != 5);
+
+        sc.close();
     }
 
     public static void menu () {
+        System.out.println("\n\t| MENU DE OPCIONES |");
         System.out.println("1. Insertar elemento");
         System.out.println("2. Eliminar elemento");
         System.out.println("3. Mostrar Tabla");
-        System.out.println("4. Salir del Programa");
+        System.out.println("4. Buscar elemento");
+        System.out.println("5. Salir del Programa\n");
     }
 }
